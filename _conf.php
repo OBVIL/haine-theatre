17609894578a621b8fea7524bdf41692ae62661f
glorieux-f <frederic.glorieux@fictif.org>
2017-09-21 19:44:44 +0200

    Mise à jour liens schémas

--- _conf.php
+++ _conf.php
@@ -2,7 +2,7 @@
 return array(
   "title" => "Haine du théâtre", // Nom du corpus
   "srcdir" => dirname( __FILE__ ), // dossier source depuis lequel exécuter la commande de mise à jour
-  "cmdup" => "git pull", // commande de mise à jour
+  "cmdup" => "git pull 2>&1", // commande de mise à jour
   "pass" => "", // Mot de passe à renseigner obligatoirement à l’installation
   "srcglob" => array( "xml/*.xml", "xml-diplo/*.xml" ), // sources XML à publier
   "sqlite" => "hdt.sqlite", // nom de la base avec les métadonnées
